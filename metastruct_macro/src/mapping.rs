5b5a547ad654353689b3f6b113603d02da284eea
Michael Sproul <michael@sigmaprime.io>
2024-10-24 12:58:47 +1100

    Lifetime tweaks

--- metastruct_macro/src/mapping.rs
+++ metastruct_macro/src/mapping.rs
@@ -33,9 +33,9 @@ pub(crate) fn generate_mapping_macro(
         .iter()
         .map(|field_type| {
             if mapping_opts.mutable {
-                quote! { &mut dyn FnMut(usize, &'_ mut #field_type) -> _ }
+                quote! { mut #field_type }
             } else {
-                quote! { &mut dyn FnMut(usize, &'_ #field_type) -> _ }
+                quote! { #field_type }
             }
         })
         .collect::<Vec<_>>();
@@ -54,7 +54,7 @@ pub(crate) fn generate_mapping_macro(
     quote! {
         #[macro_export]
         macro_rules! #macro_name {
-            ($v:expr, $f:expr) => {
+            (&$lifetime:tt _, $v:expr, $f:expr) => {
                 match $v {
                     #type_name {
                         #(
@@ -64,13 +64,16 @@ pub(crate) fn generate_mapping_macro(
                     } => {
                         let mut __metastruct_i: usize = 0;
                         #(
-                            let __metastruct_f: #mapping_function_types = &mut $f;
+                            let __metastruct_f: &mut dyn FnMut(usize, &$lifetime #mapping_function_types) -> _ = &mut $f;
                             #function_call_exprs;
                             __metastruct_i += 1;
                         )*
                     }
                 }
-            }
+            };
+            ($v:expr, $f:expr) => {
+                #macro_name!(&'_ _, $v, $f)
+            };
         }
     }
     .into()
